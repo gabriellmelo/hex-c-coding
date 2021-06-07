b9d7bbb1ce6cb22b97d59a277340b270d0bc007a
gabriellmelo <gabriellucca.mello@outlook.com>
2021-06-07 18:59:05 +0000

    with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/2-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main - entry point
+ *
+ * Return: always 0, success
+ */
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
