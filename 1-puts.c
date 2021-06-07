ab50c30eae73c3c968bd5cb668bbae84c26aafbe
gabriellmelo <gabriellucca.mello@outlook.com>
2021-06-07 18:38:56 +0000

    Programming is like building a multilingual puzzle

---
+++ 1-puts.c
@@ -0,0 +1,15 @@
+/**
+ * main - entry point
+ *
+ * Return: always 0, success
+ */
+
+
+#include<stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
